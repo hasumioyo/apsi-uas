e060fe109501dee89d6274cf6897d99675569fa4
Stefanifani <stefanifransiska88@gmail.com>
2025-12-18 13:54:49 +0700

    ubah isi tabel di user management

--- users-index.php
+++ users-index.php
@@ -15,21 +15,21 @@
                                 <tr class="text-dark">
                                     <th scope="col"><input class="form-check-input" type="checkbox"></th>
                                     <th scope="col">Date</th>
-                                    <th scope="col">Invoice</th>
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Amount</th>
-                                    <th scope="col">Status</th>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Item</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Min.Stock</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
+                                    <td>01 Jan 2026</td>
+                                    <td>0001</td>
+                                    <td>Chitato</td>
+                                    <td>100</td>
+                                    <td>5</td>
                                     <td>
                                         <a class="btn btn-sm btn-success" href=""><i class="fa fa-edit"></i></a>
                                         <a class="btn btn-sm btn-danger" href=""><i class="fa fa-trash-alt"></i></a>
@@ -37,38 +37,38 @@
                                 </tr>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
+                                    <td>01 Jan 2026</td>
+                                    <td>0002</td>
+                                    <td>Pringles</td>
+                                    <td>90</td>
+                                    <td>5</td>
                                     <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
                                 </tr>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
+                                    <td>01 Jan 2026</td>
+                                    <td>0003</td>
+                                    <td>Indomie</td>
+                                    <td>50</td>
+                                    <td>5</td>
                                     <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
                                 </tr>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
+                                    <td>01 Jan 2026</td>
+                                    <td>0004</td>
+                                    <td>Lays</td>
+                                    <td>80</td>
+                                    <td>5</td>
                                     <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
                                 </tr>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
+                                    <td>01 Jan 2026</td>
+                                    <td>0005</td>
+                                    <td>Oreo</td>
+                                    <td>35</td>
+                                    <td>5</td>
                                     <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
                                 </tr>
                             </tbody>
