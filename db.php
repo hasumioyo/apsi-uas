0286209e57ae0a5cdf384236ea19418d8629c441
hasumioyo <hasumioyozakie@gmail.com>
2026-01-12 20:35:16 +0700

    make sign up only

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "cahaya_frans"; //sesuaikan nama db yang telah dibuat
+
+    $connect = mysqli_connect($host,$user,$pass,$db);
